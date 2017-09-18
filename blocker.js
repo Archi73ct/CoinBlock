1c81cabe52eb4036bd052dc7a106966aaaa735e2
Jacob Bech <jbechsem@gmail.com>
2017-09-18 13:02:12 +0200

    Added captcha url

--- blocker.js
+++ blocker.js
@@ -1,14 +1,16 @@
 var scriptList = document.getElementsByTagName('script');
-var matchingPattern = /https:\/\/coin\-hive\.com\/lib\/*/;
+var matchingPatternMiner = /https:\/\/coin\-hive\.com\/lib\/*/;
+var matchingPatternCapthca = /https:\/\/coin\-hive\.com\/capthca\/*/;
 
 console.log("[CoinBlock] Found scripts: " + scriptList.length);
 
 for (var i = scriptList.length - 1; i >= 0; i--) {
-	if (matchingPattern.test(scriptList[i].src)) {
+	if (matchingPatternMiner.test(scriptList[i].src) | matchingPatternCapthca.test(scriptList[i].src)) {
 		console.log(scriptList[i].src);
-		scriptList[i].setAttribute("src", "");
+		scriptList[i].src = "none";
 		console.log(scriptList[i].src);
 		console.log("[CoinBlock] Found content script");
 		}
 }
 
+d
