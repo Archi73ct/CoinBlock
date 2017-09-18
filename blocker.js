c293954b6b23a5020103428efa7adf8e0e3b2964
Jacob Bech <jbechsem@gmail.com>
2017-09-18 16:29:06 +0200

    Block Iframes, debugged

--- blocker.js
+++ blocker.js
@@ -1,16 +1,29 @@
 var scriptList = document.getElementsByTagName('script');
+var iframeList = document.getElementsByTagName('iframe');
+
 var matchingPatternMiner = /https:\/\/coin\-hive\.com\/lib\/*/;
-var matchingPatternCapthca = /https:\/\/coin\-hive\.com\/capthca\/*/;
+var matchingPatternCapthca = /https:\/\/coin\-hive\.com\/captcha\/*/;
 
 console.log("[CoinBlock] Found scripts: " + scriptList.length);
+console.log("[CoinBlock] Found iframes: " + iframeList.length);
 
-for (var i = scriptList.length - 1; i >= 0; i--) {
-	if (matchingPatternMiner.test(scriptList[i].src) | matchingPatternCapthca.test(scriptList[i].src)) {
-		console.log(scriptList[i].src);
-		scriptList[i].src = "none";
-		console.log(scriptList[i].src);
-		console.log("[CoinBlock] Found content script");
-		}
+if (scriptList.length >= 1) {
+	for (var i = scriptList.length - 1; i >= 0; i--) {
+		if (matchingPatternMiner.test(scriptList[i].src) | matchingPatternCapthca.test(scriptList[i].src)) {
+			console.log(scriptList[i].src);
+			scriptList[i].src = "none";
+			console.log(scriptList[i].src);
+			console.log("[CoinBlock] Found content script");
+			}
+	}
 }
-
-d
+if (iframeList.length >= 1) {
+	for (var i = iframeList.length - 1; i >= 0; i--) {
+		if (matchingPatternMiner.test(iframeList[i].src) | matchingPatternCapthca.test(iframeList[i].src)) {
+			console.log(iframeList[i].src);
+			iframeList[i].src = "none";
+			console.log(iframeList[i].src);
+			console.log("[CoinBlock] Found content script");
+			}
+	}
+}
