e7ed9f417a731d8fb634aa5566b460d341338c79
Bechsen <jbechsen@gmail.com>
2017-09-22 18:16:35 +0200

    Now 2.0 Using webRequest blocking

--- background.js
+++ background.js
@@ -1,29 +1,10 @@
-var active_default = false;
-var active = null;
+var active = true;
 var pattern = "https://coin-hive.com/*";
 
 function redirect(requestDetails) {
-	console.log("Cancelled bitch!");
 	return {
-		
 		cancel: true
 	};
-}
-
-function updateState(message, sender, sendResponse) {
-	if(message.cmd == "switch") {
-		console.log("memed");
-
-		if(active == true){ active = false; }
-
-		else if(active == false) { active = true; }
-
-		else{active=active_default;}
-	}
-
-	if(message.cmd == "requeststate"){
-		sendResponse({response: active});
-	}
 }
 
 browser.webRequest.onBeforeRequest.addListener(
@@ -32,4 +13,8 @@ browser.webRequest.onBeforeRequest.addListener(
 	["blocking"]
   );
 
-browser.runtime.onMessage.addListener(updateState);
+function switch_active () {
+	
+}
+
+browser.browserAction.onClicked.addListener(switch_active);
--- popup/menu.js
+++ popup/menu.js
@@ -1,5 +1,4 @@
 function checkState(message){
-	console.log("Respone recieved: " + message.response);
 	if(message.response == true) {
 		document.getElementById('activator').style.backgroundColor="#12bc00";
 		document.getElementById('activator').innerHTML = "Activated";
@@ -13,13 +12,12 @@ function checkState(message){
 
 
 browser.runtime.sendMessage({"cmd": "requeststate"}).then(checkState);
-console.log("Cheking state");
+
 
 document.addEventListener("click", function(e) {
 	if(!e.target.classList.contains("action")){
 		return;
 	}
-	console.log("clicked activete");
 	browser.runtime.sendMessage({"cmd": "switch"});
 	browser.runtime.sendMessage({"cmd": "requeststate"}).then(checkState);
 	
